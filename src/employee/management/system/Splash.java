4c44b566a3227b1900d185c31cf7a19e451f9d58
Atom M. Nava <atommunoznava@gmail.com>
2024-08-21 20:46:20 -0600

    Nueva clase Splash

--- src/employee/management/system/Splash.java
+++ src/employee/management/system/Splash.java
@@ -1,2 +1,36 @@
-package employee.management.system;public class Splash {
+
+package employee.management.system;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Splash extends JFrame {
+    Splash()
+    {
+        ImageIcon image1 = new ImageIcon(ClassLoader.getSystemResource("icons/front.gif"));
+        Image image2 = image1.getImage().getScaledInstance(1170, 650, Image.SCALE_DEFAULT);
+        ImageIcon image3 = new ImageIcon(image2);
+        JLabel image = new JLabel(image3);
+        image.setBounds(0,0,1170,650);
+        add(image);
+
+        setSize(1170,650);
+        setLocationRelativeTo(null);
+        setLayout(null);
+        setVisible(true);
+
+        try {
+            Thread.sleep(5000);
+            setVisible(false);
+            new Login().setVisible(true);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public static void main(String[] args) {
+        new Splash();
+    }
 }
+
+
+
