e30639c3cec6ef79f8f2ea598b965d2c7f4e4491
Atom M. Nava <atommunoznava@gmail.com>
2024-08-22 12:53:28 -0600

    Nueva ca

---
+++ src/employee/management/system/Main.java
@@ -0,0 +1,14 @@
+package employee.management.system;
+
+import javax.swing.*;
+
+public class Main extends JFrame {
+
+    Main(){
+        setSize(500,500);
+        setVisible(true);
+    }
+    public static void main(String[] args) {
+        new Main();
+    }
+}
