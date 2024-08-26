ef20d94a2001b6aaadd4120f179f5dbb91750847
Atom M. Nava <atommunoznava@gmail.com>
2024-08-26 17:10:40 -0600

    Documetación FINAL Conn

--- src/employee/management/system/Conn.java
+++ src/employee/management/system/Conn.java
@@ -1,10 +1,23 @@
 package employee.management.system;
 
+// Bibliotecas
 import java.sql.*;
 
+/*
+ * @brief La clase 'Conn' conecta nuestra base de datos de MySQL llamada 'employeemanagementsystem', esta contiene las mesas
+ *        'login' y 'employee' con sus respectivos campos, y actualiza nuestra base de datos.
+ * @author Atom Alexander M. Nava
+ * @date 26/08/24
+ */
 public class Conn {
+    // Variables
     Connection c;
     Statement s;
+    /*
+     * @brief Contructor 'Conn' para realizar las conexiones requeridas con nuestra base de datos en MySQL
+     * @author Atom Alexander M. Nava
+     * @date 26/08/24
+     */
     public Conn() {
         try {
             //Class.forName("com.mysql.jdbc.Driver");
